c1c6d3fd6f11ce74617291adf7d14331787a5718
Graziaczek <66276396+Graziaczek@users.noreply.github.com>
2023-07-05 18:30:07 +0200

    Trochę z bazą danych

--- Projekt krzyżówki/Models/InstrukcjeDataBase.cs
+++ Projekt krzyżówki/Models/InstrukcjeDataBase.cs
@@ -1,6 +1,37 @@
-﻿namespace Projekt_krzyżówki.Models
+﻿using System.Data.SQLite;
+namespace Projekt_krzyżówki.Models
 {
     public class InstrukcjeDataBase
     {
+        static string FindSolutionFilePath(string directory) {                                                    
+            while (!string.IsNullOrEmpty(directory))
+            {
+                string[] solutionFiles = Directory.GetFiles(directory, "*.db");
+                if (solutionFiles.Length > 0)
+                {
+                    return solutionFiles[0];
+                }
+                directory = Directory.GetParent(directory)?.FullName;
+            }
+            return null;
+        }
+        static string datasource = "Datasource=" + FindSolutionFilePath(Directory.GetCurrentDirectory()) + ";Version=3";
+        SQLiteConnection conn = new SQLiteConnection(datasource);
+        public void ReadDataBase()
+        {
+
+        }
+        public void AddQuestion()
+        {
+
+        }
+        public void AddAnswer()
+        {
+
+        }
+        public void AddAnagram()
+        {
+
+        }
     }
 }
